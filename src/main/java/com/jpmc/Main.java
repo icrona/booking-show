1fb6f2cc1ae72a6f2cd95a6809a38c818838b57c
Aldo Makmur <aldomakmur@gmail.com>
2023-03-31 22:29:02 +0800

    application prompt scanner and output

--- src/main/java/com/jpmc/Main.java
+++ src/main/java/com/jpmc/Main.java
@@ -1,7 +1,31 @@
 package com.jpmc;
 
+import com.jpmc.command.factory.CommandFactory;
+import com.jpmc.exception.BusinessException;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(true) {
+            System.out.println("Select User (1)Admin (2)Buyer : ");
+            int user = new Scanner(System.in).nextInt();
+
+            if(user != 1 && user != 2) {
+                System.out.println("Invalid user");
+                continue;
+            }
+
+            try {
+                System.out.println("Enter Command : ");
+                String command = new Scanner(System.in).nextLine();
+                String output = CommandFactory.getCommand(user, command).execute();
+                System.out.println(output);
+            }
+            catch (BusinessException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+        }
     }
 }
